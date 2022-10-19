126415b837997a9ac17b914af69498fa10b67599
oskla <oskarlarssson@gmail.com>
2022-10-19 09:36:21 +0200

    Initial Commit

---
+++ MapLecture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapLecture
+//
+//  Created by Oskar Larsson on 2022-10-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapLecture/MapLectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapLectureApp.swift
+//  MapLecture
+//
+//  Created by Oskar Larsson on 2022-10-19.
+//
+
+import SwiftUI
+
+@main
+struct MapLectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
